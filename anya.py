93ab6d15a6263197d090a401b5a06cc585f09559
anyaknopova <annilik22@gmail.com>
2023-11-26 16:02:26 +0700

    dragon

--- anya.py
+++ anya.py
@@ -1,6 +1,9 @@
 import turtle
 
-'''
+
+turtle.speed(-1)
+
+
 def square(n):
     for i in range(4):
         turtle.fd(n)
@@ -16,57 +19,6 @@ def rec_square(n):
     square(n)
     turtle.rt(5)
     return rec_square(0.9 * n)
-
-
-rec_square(100)
-turtle.done()
-
-
-def koch(order, size):
-    if order == 0:
-        forward(size)
-    else:
-        koch(order-1, size/3)
-        left(60)
-        koch(order-1, size/3)
-        right(120)
-        koch(order-1, size/3)
-        left(60)
-        koch(order-1, size/3)
-
-
-def main():
-    tracer(0)
-    up()
-    goto(-300, 0)
-    down()
-    n = int(input('Глубина рекурсии:'))
-    a = int(input('Длина стороны:'))
-    koch(n, a)
-    update()
-    done()
-
-main()
-
-
-def squares(k, size):
-    if k == 0:
-        for i in range(4):
-            turtle.forward(size)
-            turtle.rt(90)
-
-    else:
-        turtle.pu()
-        turtle.forward(0.05*size)
-        turtle.rt(10)
-        turtle.pd()
-        return squares(k-1, size*0.9)
-
-
-squares(3, 200)
-'''
-
-turtle.speed(-1)
 
 
 def levy(n_l, size_l):
@@ -91,7 +43,7 @@ def dragon(n_d, size_d, sign):
         turtle.lt(45*sign)
 
 
-dragon(10, 100, 1)
+dragon(12, 300, 1)
 
 
 
