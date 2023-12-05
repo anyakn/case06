9c9c8a156a8bfed94f888faa10c143a6cdf64f1d
Ba1an <karolina_karo_balan@mail.ru>
2023-12-05 14:43:45 +0700

    karolina's

--- karol.py
+++ karol.py
@@ -1,14 +1,17 @@
 import turtle
 
-turtle.speed(1)
-'''
+turtle.speed(100)
+depth = int(input('Глубина рекурсии:'))
+size = int(input('Длина стороны:'))
+
+
 def dandelion(size, depth):
     if depth == 0:
         turtle.forward(size)
     else:
         turtle.forward(size)
         for i in range(6):
-            dandelion(size/3, depth-1)
+            dandelion(size / 3, depth - 1)
             turtle.right(60)
         turtle.backward(size)
 
@@ -18,27 +21,20 @@ def dandelion_snowflake(size, depth):
         dandelion(size, depth - 1)
         turtle.right(60)
 
-depth = int(input('Глубина рекурсии:'))
-size = int(input('Длина стороны:'))
-
 dandelion_snowflake(size, depth)
-turtle.done()
 
-
-import turtle
-turtle.speed(1)
 
 def koch(size, depth):
     if depth == 0:
         turtle.forward(size)
     else:
-        koch(size/3, depth-1)
+        koch(size / 3, depth - 1)
         turtle.left(60)
-        koch(size/3, depth-1)
+        koch(size / 3, depth - 1)
         turtle.right(120)
-        koch(size/3, depth-1)
+        koch(size / 3, depth - 1)
         turtle.left(60)
-        koch(size/3, depth-1)
+        koch(size / 3, depth - 1)
 
 
 def koch_snowflake(size, depth):
@@ -47,40 +43,40 @@ def koch_snowflake(size, depth):
         turtle.right(120)
 
 
+koch_snowflake(size, depth)
+
+
 def ice_1(size, depth):
     if depth == 0:
         turtle.forward(size)
     else:
-        ice_1(size, depth-1)
+        ice_1(size, depth - 1)
         turtle.left(90)
         ice_1(size / 2, depth - 1)
         turtle.right(180)
         ice_1(size / 2, depth - 1)
         turtle.left(90)
-        ice_1(size, depth-1)
+        ice_1(size, depth - 1)
 
 
-depth = int(input('Глубина рекурсии:'))
-size = int(input('Длина стороны:'))
 ice_1(size, depth)
 
-koch_snowflake(size, depth)
 
-'''
 def color_tree(depth, size):
-  turtle.colormode(255)
-  cg = 255 - int(depth * (250/6)) % 255
-  turtle.color(0, cg, 0)
-  if depth == 0:
-    return
-  else:
-    turtle.forward(size)
-    turtle.left(45)
-    color_tree(depth-1, size / 2)
-    turtle.right(90)
-    color_tree(depth-1, (size/2))
-    turtle.left(45)
-    turtle.backward(size)
+    turtle.colormode(255)
+    cg = 255 - int(depth * (250 / 6)) % 255
+    turtle.color(0, cg, 0)
+    if depth == 0:
+        return
+    else:
+        turtle.forward(size)
+        turtle.left(45)
+        color_tree(depth - 1, size / 2)
+        turtle.right(90)
+        color_tree(depth - 1, (size / 2))
+        turtle.left(45)
+        turtle.backward(size)
+
 
 turtle.left(90)
 color_tree(depth, size)
